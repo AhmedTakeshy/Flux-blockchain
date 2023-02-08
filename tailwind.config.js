a263909468479297592e948674d697d364a9175b
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2023-02-08 03:44:15 +0300

    Create blockchain app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#16a085",
+        yellow: "#f4d03f",
+      },
+    },
+  },
+  plugins: [],
+};
