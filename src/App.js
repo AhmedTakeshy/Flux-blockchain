4e81b9abc1154a7e99446957f464c847bf3371c7
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2023-02-09 17:08:46 +0300

    Make component uppercase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import About from "./components/About/About";
-import Banner from "./components/Banner";
-import Developers from "./components/Developers";
-import Footer from "./components/Footer/Footer";
-import Nav from "./components/Nav";
-import Subscribe from "./components/Subscribe";
+import About from "./Components/About/About";
+import Banner from "./Components/Banner";
+import Developers from "./Components/Developers";
+import Footer from "./Components/Footer/Footer";
+import Nav from "./Components/Nav";
+import Subscribe from "./Components/Subscribe";
 function App() {
   return (
     <div>
